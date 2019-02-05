944c3d13d83f94803bd2c3caf7fddfe47a56a261
Eli Friedman <friedm3@cooper.edu>
2019-02-05 11:48:58 +0200

    make variable private

--- cyberml/graph.py
+++ cyberml/graph.py
@@ -3,11 +3,16 @@ import igraph
 
 class Graph(object):
     def __init__(self):
-        self.edges = set()
-        self.nodes = set()
+        self._edges = set()
+        self._nodes = set()
         self._name2id = {}
+        self._id2name = {}
         self._networkx = None
         self._igraph = None
+
+    @classmethod
+    def load_from_db(cls, graph_id):
+        return cls.load_from_csv("_graphs/"+graph_id)  # TODO don't hardcode
 
     @classmethod
     def load_from_csv(cls, filename):
@@ -20,21 +25,35 @@ class Graph(object):
                 w = edgeinfo[2] if len(edgeinfo) >= 3 else 1
                 if node1 not in g._name2id:
                     g._name2id[node1] = id
+                    g._id2name[id] = node1
                     id += 1
                 if node2 not in g._name2id:
                     g._name2id[node2] = id
+                    g._id2name[id] = node2
                     id += 1
                 node1 = g._name2id[node1]  # igraph requires ids rather than names
                 node2 = g._name2id[node2]
-                g.nodes.add(node1)
-                g.nodes.add(node2)
-                g.edges.add((node1, node2, w))
+                g._nodes.add(node1)
+                g._nodes.add(node2)
+                g._edges.add((node1, node2, w))
         return g
+
+    @property
+    def edges(self):
+        edges = []
+        for edge in self._edges:
+            v1, v2, w = edge
+            edges.append((
+                self._id2name[v1],
+                self._id2name[v2],
+                w
+            ))
+        return edges
 
     def get_networkx(self):
         if self._networkx is None:
             self._networkx = nx.Graph()
-            for v1, v2, w in self.edges:
+            for v1, v2, w in self._edges:
                 self._networkx.add_edge(v1, v2, w=w)
         return self._networkx
 
@@ -42,7 +61,7 @@ class Graph(object):
         if self._igraph is None:
             self._igraph = igraph.Graph()
             self._igraph.add_vertices(range(len(self._name2id)))
-            self._igraph.add_edges([(v1, v2) for v1, v2, w in self.edges])
-            for v1, v2, w in self.edges:
+            self._igraph.add_edges([(v1, v2) for v1, v2, w in self._edges])
+            for v1, v2, w in self._edges:
                 self._igraph[v1, v2, 'w'] = w
         return self._igraph
--- cyberml/main.py
+++ cyberml/main.py
@@ -4,23 +4,16 @@ import time
 
 from .graph import Graph
 from .features.feature_list import featurizer_list, featurize
+from .train import train
 
-
-def main():
-    parseargs = argparse.ArgumentParser()
-#    parseargs.add_argument("filename", type=str, help="add an input graph csv file in edge list format")
-    parseargs.add_argument("-o", "--output", type=str, default="output.csv", help="add an output file name")
-    args = parseargs.parse_args()
-
+def _load_graphs():
     dirn = "_graphs/"
     graphnames = os.listdir(dirn)
-    f = open(args.output, 'w')
-    n = 0
-    for i, graphname in enumerate(graphnames):
-        print("processing graph {}/{}".format(i, len(graphnames)), end=", ")
-        sl = time.time()
-        graph = Graph.load_from_csv(dirn+graphname)
-        print("load={}".format(time.time() - sl), end=", ")
+    return [Graph.load_from_csv(dirn+graphname) for graphname in graphnames]
+
+def _update_features(output_file, graphs):
+    f = open(output_file, 'w')
+    for i, graph in enumerate(graphs):
         sf = time.time()
         features = featurize(featurizer_list, graph)
         print("feat={}".format(time.time() - sf))
@@ -28,9 +21,23 @@ def main():
         vals = [feat.value for feat in features]
         if i == 0:
             f.write(",".join(names)+"\n")
-        f.write(graphname+"," + ",".join(["%.3f" % val for val in vals]) + "\n")
+        f.write(graphname+"," + ",".join([str(val) for val in vals]) + "\n")
         f.flush()
     f.close()
+
+def main():
+   parseargs = argparse.ArgumentParser()
+   parseargs.add_argument("-o", "--output", type=str, default="output.csv", help="add an output file name")
+   parseargs.add_argument("--features", action="store_true")
+   args = parseargs.parse_args()
+
+   graphs = _load_graphs()
+   if args.features:
+       _update_features(args.output, graphs)
+   else:
+       train(graphs)
+
+
 
 
 if __name__ == '__main__':
